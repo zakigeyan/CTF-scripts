92a2dc59adc579b1769d42f480d2d6363ac93bdb
zakigeyan <zakig013@gmail.com>
2021-04-03 20:14:40 +0700

    update LCG: test

--- Linear_Congruential_Generator/LCG.py
+++ Linear_Congruential_Generator/LCG.py
@@ -13,15 +13,31 @@ class LCG:
         return self.seed
 
 class CrackLCG:
-    def __init__(self, modulus, states):
-        self.modulus = modulus
-        assert len(states) >= 3, 'insufficient states'
+    def __init__(self, states):
         self.states = states
+        diffs = [s1 - s0 for s0, s1 in zip(states, states[1:])]
+        zeros = [t2 * t0 - t1 * t1 for t0, t1, t2 in zip(diffs, diffs[1:], diffs[2:])]
+        self.modulus = zeros[0]
+        for i in zeros[1:]:
+            self.modulus = GCD(self.modulus, i)
         self.a = (self.states[2] - self.states[1]) * int(inverse(self.states[1] - self.states[0], self.modulus)) % self.modulus
         self.c = (self.states[1] - self.states[0] * self.a) % self.modulus
     def genPrevState(self):
         self.states.insert(0, (self.states[0] - self.c) * inverse(self.a, self.modulus) % self.modulus)
     def genNextState(self):
         self.states.append((self.a * self.states[-1] + self.c) % self.modulus)
-    def getStates():
+    def getStates(self):
         return self.states
+
+def testLCG(num=10):
+    mod = getPrime(64)
+    a = randrange(1, mod-1)
+    c = randrange(1, mod-1)
+    seed = randrange(1, mod-1)
+    L = LCG(mod, a, c, seed)
+    states = [L.next() for _ in range(num)]
+    CL = CrackLCG(states)
+    for _ in range(num):
+        CL.genNextState()
+        states.append(L.next())
+    assert CL.getStates() == states
